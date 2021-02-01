32db179edb6d15e3a2721c3d149ffbea7c047bdf
shriyadita31 <74048787+shriyadita31@users.noreply.github.com>
2021-02-01 13:39:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+var ground;
+var bin1,bin2,bin3,binImg;
+var backgroundIMG;
+
+function preload() {
+	binImg = loadImage("dustbingreen.png");
+	
+}
+
+function setup() {
+	
+	createCanvas(800, 700);
+	
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ball = new paperBall();
+
+	ground = Bodies.rectangle(width/2,600,800,20,{isStatic: true});
+	World.add(world,ground);
+
+	bin1 = new dustbin(660,580,150,20);
+	bin2 = new dustbin(575,520,20,140);
+	bin3 = new dustbin(745,520,20,140);
+
+	Engine.run(engine);
+	//console.log(ball);
+
+	//console.log(ball.body.position);
+}
+
+
+function draw() {
+	rectMode(CENTER);
+	background(229,229,229);
+	
+
+	ball.display();
+	
+	fill(255,255,255);
+	rect(ground.position.x,ground.position.y,800,20);
+
+	//bin1.display();
+	//bin2.display();
+	//bin3.display();
+
+	imageMode(CENTER);
+	image(binImg,665	,520,160,144);
+
+	drawSprites();
+
+	//fill("black");
+	//text(mouseX+","+mouseY,200,200);
+
+
+
+	if(keyDown(UP_ARROW) && ball.body.position.y>530) {
+		Body.applyForce(ball.body,ball.body.position,{x:520,y:-540})
+	}
+
+	Engine.update(engine);
+}
